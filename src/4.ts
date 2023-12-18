2b54f5d6eaee4a2d842ab9469b57acc66d4d6a0d
VadymKrem <122228240+VadymKrem@users.noreply.github.com>
2023-12-18 23:51:56 +0200

    18-12
    homework
    4tasks

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,59 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  private tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Вхід до будинку");
+    } else {
+      console.log("Двері зачинені");
+    }
+  }
+}
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Двері відчиняються");
+    } else {
+      console.log("Двері зачинені");
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
-
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
